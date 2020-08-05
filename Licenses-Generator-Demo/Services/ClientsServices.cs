a9e8363de996c96c505d2d444dec1bf06b18652d
Msoliman0512 <msoliman@silverkeytech.com>
2020-08-05 15:42:03 +0200

    Add ClientsService

---
+++ Licenses-Generator-Demo/Services/ClientsServices.cs
@@ -0,0 +1,53 @@
+﻿using Licenses_Generator_DBFirst.DatabaseSpecific;
+using Licenses_Generator_DBFirst.EntityClasses;
+using Licenses_Generator_DBFirst.Linq;
+using Microsoft.AspNetCore.Hosting;
+using SD.LLBLGen.Pro.LinqSupportClasses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Licenses_Generator_Demo.Services
+{
+    public class ClientsServices
+    {
+        public ClientsServices(IWebHostEnvironment webHostEnvironment)
+        {
+            WebHostEnvironment = webHostEnvironment;
+        }
+
+        public IWebHostEnvironment WebHostEnvironment { get; }
+
+        public async Task<IEnumerable<ClientEntity>> GetClientsAsync()
+        {
+
+            IEnumerable<ClientEntity> clients = null;
+            using (var adapter = new DataAccessAdapter())
+            {
+                var metaData = new LinqMetaData(adapter);
+                var query = metaData.Client;
+                clients = await query.ToListAsync();
+            }
+            return clients;
+        }
+
+        public IEnumerable<ClientEntity> GetClients()
+        {
+            using (var adapter = new DataAccessAdapter())
+            {
+                var metaData = new LinqMetaData(adapter);
+                adapter.CloseConnection();
+                return metaData.Client.ToList();
+            }
+        }
+
+        public void SaveClient(ClientEntity clientEntity)
+        {
+            using (var adapter = new DataAccessAdapter())
+            {
+                adapter.SaveEntity(clientEntity);
+            }
+        }
+    }
+}
