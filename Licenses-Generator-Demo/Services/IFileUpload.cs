b46661ae0f05286298a09b22fb1368172b9e7a4d
Msoliman0512 <msoliman@silverkeytech.com>
2020-08-13 15:43:17 +0200

    Add IFileUpload Service

---
+++ Licenses-Generator-Demo/Services/IFileUpload.cs
@@ -0,0 +1,15 @@
+﻿using BlazorInputFile;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Licenses_Generator_Demo.Services
+{
+    public interface IFileUpload
+    {
+
+
+        Task Upload(IFileListEntry file);
+    }
+}
