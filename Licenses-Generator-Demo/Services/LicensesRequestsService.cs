d25d8556fe1a1c10a3c01ea78c8b0d9e2c12b17e
Msoliman0512 <msoliman@silverkeytech.com>
2020-08-18 17:52:02 +0200

    Create LicensesRequestsService

---
+++ Licenses-Generator-Demo/Services/LicensesRequestsService.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using Licenses_Generator_DBFirst.DatabaseSpecific;
+using Licenses_Generator_DBFirst.EntityClasses;
+using Licenses_Generator_DBFirst.Linq;
+using Microsoft.AspNetCore.Hosting;
+using SD.LLBLGen.Pro.LinqSupportClasses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Licenses_Generator_Demo.Services
+{
+    public class LicensesRequestsService
+    {
+        public LicensesRequestsService(IWebHostEnvironment webHostEnvironment)
+        {
+            WebHostEnvironment = webHostEnvironment;
+        }
+
+        public IWebHostEnvironment WebHostEnvironment { get; }
+
+        public async Task<IEnumerable<LicenseRequestEntity>> GetLicensesRequestsAsync()
+        {
+
+            IEnumerable<LicenseRequestEntity> requests = null;
+            using (var adapter = new DataAccessAdapter())
+            {
+                var metaData = new LinqMetaData(adapter);
+                var query = metaData.LicenseRequest;
+                requests = await query.ToListAsync();
+            }
+            return requests;
+        }
+
+        public IEnumerable<LicenseRequestEntity> GetLicensesRequests()
+        {
+            using (var adapter = new DataAccessAdapter())
+            {
+                var metaData = new LinqMetaData(adapter);
+                adapter.CloseConnection();
+                return metaData.LicenseRequest.ToList();
+            }
+        }
+
+        public async Task<bool> SaveLicenseRequest(LicenseRequestEntity licenseRequestEntity)
+        {
+            using (var adapter = new DataAccessAdapter())
+            {
+                return await adapter.SaveEntityAsync(licenseRequestEntity, refetchAfterSave: true);
+            }
+        }
+    }
+}
